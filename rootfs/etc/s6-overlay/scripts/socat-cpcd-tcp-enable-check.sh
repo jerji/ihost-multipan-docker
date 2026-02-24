04d4934b4405bda60166f24f76ec61024c593b59
Florian Heese (Admin) <fhadmin@nexus.dd.de.heese.earth>
2026-02-24 21:47:47 +0100

    fix Enable socat-cpcd-tcp service if needed

--- rootfs/etc/s6-overlay/scripts/socat-cpcd-tcp-enable-check.sh
+++ rootfs/etc/s6-overlay/scripts/socat-cpcd-tcp-enable-check.sh
@@ -2,9 +2,10 @@
 # ==============================================================================
 # Enable socat-cpcd-tcp service if needed
 # ==============================================================================
+source /etc/bashlog/log.sh;
 
-# if bashio::config.has_value 'network_device'; then
-#     touch /etc/s6-overlay/s6-rc.d/user/contents.d/socat-cpcd-tcp
-#     touch /etc/s6-overlay/s6-rc.d/cpcd/dependencies.d/socat-cpcd-tcp
-#     bashio::log.info "Enabled socat-cpcd-tcp."
-# fi
+if [ -n "${NETWORK_DEVICE}" ]; then
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/socat-cpcd-tcp
+    touch /etc/s6-overlay/s6-rc.d/cpcd/dependencies.d/socat-cpcd-tcp
+    log 'info' "Enabled socat-cpcd-tcp."
+fi
